c95bb587c16a0786f0e50485cdc632b0e9df16e8
Nick Chapman <nic@amadido.co.uk>
2020-12-01 05:14:24 +0000

    day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,18 +1,21 @@
 
 module Day1 where --(main) where
 
+import Data.List (nub)
 import Misc
 
 main :: IO ()
 main = do
+  let sam1 = [1721, 979, 366, 299, 675, 1456]
+  print ("day1, sam", check [514579] $ part1 sam1)
   xs <- readInts "input/day1.input"
-  print xs
-  print $ length xs
-  print ("day1, example1", check 123 $ calc1 1000)
-  --print ("day1, part1", check 99 $ part1 xs)
+  print ("day1, part1", check [788739] $ part1 xs)
+  print ("day1, part2", check [178724430] $ part2 xs)
 
-part1 :: [Int] -> Int
-part1 = undefined calc1
+part1 :: [Int] -> [Int]
+part1 xs =
+  nub [ a * b |  a <- xs, b <- xs, a + b == 2020 ]
 
-calc1 :: Int -> Int
-calc1 = undefined
+part2 :: [Int] -> [Int]
+part2 xs =
+  nub [ a * b * c |  a <- xs, b <- xs, c <- xs, a + b + c == 2020 ]
