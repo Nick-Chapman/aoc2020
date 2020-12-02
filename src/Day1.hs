0b18a9ac606e6bcabd99aca47ad899c52192cb22
Nick Chapman <nic@amadido.co.uk>
2020-12-02 05:30:21 +0000

    tidy day1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,13 +1,11 @@
 
-module Day1 where --(main) where
+module Day1 (main) where
 
 import Data.List (nub)
-import Misc
+import Misc (check,readInts)
 
 main :: IO ()
 main = do
-  let sam1 = [1721, 979, 366, 299, 675, 1456]
-  print ("day1, sam", check [514579] $ part1 sam1)
   xs <- readInts "input/day1.input"
   print ("day1, part1", check [788739] $ part1 xs)
   print ("day1, part2", check [178724430] $ part2 xs)
