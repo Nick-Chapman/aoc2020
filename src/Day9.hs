dc861fd87f66d68ba211b21a5218083d83cb51ee
Nick Chapman <nic@amadido.co.uk>
2020-12-10 17:25:04 +0000

    rewrite 9.2 to be quadratic (almost linear) instead of cubic

--- src/Day9.hs
+++ src/Day9.hs
@@ -10,8 +10,8 @@ main = do
   inp <- parse gram <$> readFile "input/day9.input"
   let !_ = check [127] $ part1 5 sam
   print ("day9, part1", check [29221323] $ part1 25 inp)
-  let !_ = check [62] $ part2 127 sam
-  print ("day9, part2", check [4389369] $ part2 29221323 inp)
+  let !_ = check 62 $ part2 127 sam
+  print ("day9, part2", check 4389369 $ part2 29221323 inp)
   pure ()
 
 part1 :: Int -> [Int] -> [Int]
@@ -26,17 +26,14 @@ part1 n xs =
   , not b
   ]
 
-part2 :: Int -> [Int] -> [Int]
-part2 target xs =
-  [ a+b
-  | ys <- tails xs
-  , n <- [2..length ys]
-  , let sub = take n ys
-  , let m = sum sub
-  , m == target
-  , let a = minimum sub
-  , let b = maximum sub
-  ]
+part2 :: Int -> [Int] -> Int
+part2 target xs = loop [] 0 xs xs
+  where
+    loop :: [Int] -> Int -> [Int] -> [Int] -> Int
+    loop sub sum ys zs = if
+      | sum > target -> loop (tail sub) (sum - head sub) (tail ys) zs
+      | sum < target -> loop (sub ++ [head zs]) (sum + head zs) ys (tail zs)
+      | otherwise -> minimum sub + maximum sub
 
 tails :: [a] -> [[a]]
 tails xs = xs : case xs of [] -> []; _:xs -> tails xs
