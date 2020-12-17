15f6b59d2d3378b21cb4654278b22259d4bc5bea
Nick Chapman <nic@amadido.co.uk>
2020-12-17 16:55:55 +0000

    day17 - better code sharing between parts 1 and 2 (3d/4d)

--- src/Day17.hs
+++ src/Day17.hs
@@ -18,69 +18,44 @@ main = do
   print ("day17, part2", check 2056 $ part2 inp)
 
 part1 :: String -> Int
-part1 str = loop 0 (parse3 str) where
-  loop :: Int -> Set I3 -> Int
-  loop cycle active = do
-    if cycle == 6 then Set.size active else
-      loop (cycle+1) (step3 active)
+part1 = simulate init3 (step link3)
+  where init3 (x,y) = (x,y,0)
 
 part2 :: String -> Int
-part2 str = loop 0 (parse4 str) where
-  loop :: Int -> Set I4 -> Int
+part2 = simulate init4 (step link4)
+  where init4 (x,y) = (x,y,0,0)
+
+simulate :: Ord a => ((Int32,Int32) -> a) -> (Set a -> Set a) -> String -> Int
+simulate init step str = loop (0::Int) (parse init str) where
   loop cycle active = do
     if cycle == 6 then Set.size active else
-      loop (cycle+1) (step4 active)
+      loop (cycle+1) (step active)
 
+parse :: Ord a => ((Int32,Int32) -> a) -> String -> Set a
+parse init s = Set.fromList
+  [ init (x,y)
+  | (y,line) <- zip [1..] (lines s)
+  , (x,char) <- zip [1..] line
+  , case char of '#' -> True; '.' -> False; _ -> undefined ]
+
+step :: Ord a => (a -> [a]) -> Set a -> Set a
+step link active = do
+  let m = Map.fromListWith (+)
+        [ (pos',1::Int32)
+        | pos <- Set.toList active
+        , pos' <- link pos ]
+  Set.fromList
+    [ p | (p,count) <- Map.toList m
+    , count == 3 || (count == 2 && p `elem` active) ]
 
 type I3 = (Int32,Int32,Int32)
 
-delta3 :: [I3]
-delta3 = [ (x,y,z) | x <- tri, y <- tri, z <- tri, x/=0||y/=0||z/=0 ]
+link3 :: I3 -> [I3]
+link3 (a,b,c) = [ (a+x,b+y,c+z) | x <- tri, y <- tri, z <- tri, x/=0||y/=0||z/=0 ]
   where tri = [-1,0,1]
-
-add3 :: I3 -> I3 -> I3
-add3 (a,b,c) (x,y,z) = (a+x,b+y,c+z)
-
-step3 :: Set I3 -> Set I3
-step3 active = do
-  let m = Map.fromListWith (+)
-        [ (add3 pos del,1::Int32)
-        | pos <- Set.toList active
-        , del <- delta3 ]
-  Set.fromList
-    [ p | (p,count) <- Map.toList m
-    , count == 3 || (count == 2 && p `elem` active) ]
-
-parse3 :: String -> Set I3
-parse3 s = Set.fromList
-  [ (x,y,0)
-  | (y,line) <- zip [1..] (lines s)
-  , (x,char) <- zip [1..] line
-  , case char of '#' -> True; '.' -> False; _ -> undefined ]
-
 
 type I4 = (Int32,Int32,Int32,Int32)
 
-delta4 :: [I4]
-delta4 = [ (x,y,z,w) | x <- tri, y <- tri, z <- tri, w <- tri, x/=0||y/=0||z/=0||w/=0 ]
+link4 :: I4 -> [I4]
+link4 (a,b,c,d) = [ (a+x,b+y,c+z,d+w) | x <- tri, y <- tri, z <- tri, w <- tri, x/=0||y/=0||z/=0||w/=0 ]
   where tri = [-1,0,1]
-
-add4 :: I4 -> I4 -> I4
-add4 (a,b,c,d) (x,y,z,w) = (a+x,b+y,c+z,d+w)
-
-step4 :: Set I4 -> Set I4
-step4 active = do
-  let m = Map.fromListWith (+)
-        [ (add4 pos del,1::Int32)
-        | pos <- Set.toList active
-        , del <- delta4 ]
-  Set.fromList
-    [ p | (p,count) <- Map.toList m
-    , count == 3 || (count == 2 && p `elem` active) ]
-
-parse4 :: String -> Set I4
-parse4 s = Set.fromList
-  [ (x,y,0,0)
-  | (y,line) <- zip [1..] (lines s)
-  , (x,char) <- zip [1..] line
-  , case char of '#' -> True; '.' -> False; _ -> undefined ]
